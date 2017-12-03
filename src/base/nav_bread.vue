08376b29e285afe530a8bc24c59c4dbb41a0ea8a
wq93 <214180117@qq.com>
2017-12-03 15:26:18 +0800

    修改项目结构

---
+++ src/base/nav_bread.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <span>Goods</span>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
