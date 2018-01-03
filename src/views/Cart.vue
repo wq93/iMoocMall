972d842bfae944b3480137022cdd89fd521f3fe9
wq93 <214180117@qq.com>
2018-01-03 16:42:13 +0800

    前台实现-购物车页面-购物车选中0件时不能结算

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -124,7 +124,7 @@
                 Item total: <span class="total-price">{{totalPrice | currency('$')}}</span>
               </div>
               <div class="btn-wrap">
-                <a class="btn btn--red">Checkout</a>
+                <a class="btn btn--red" :class="{'btn--dis':checkedCount===0}" @click="checkOut">Checkout</a>
               </div>
             </div>
           </div>
@@ -265,6 +265,13 @@
             console.log("update suc");
           }
         })
+      },
+      checkOut() {
+        if (this.checkedCount > 0) {
+          this.$router.push({
+            path: '/address'
+          })
+        }
       }
     },
     components: {
